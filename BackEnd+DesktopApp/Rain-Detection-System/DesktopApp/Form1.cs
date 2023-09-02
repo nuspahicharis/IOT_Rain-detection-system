5f6e4fb0994023a77b083c409496a9c571bc1fba
nuspahicharis <harisnuspahic01@gmail.com>
2023-09-02 19:41:43 +0200

    Implemented DesktopApp v1
    DesktopApp with CRUD operations with assisting NuGet packages

--- BackEnd+DesktopApp/Rain-Detection-System/DesktopApp/Form1.cs
+++ BackEnd+DesktopApp/Rain-Detection-System/DesktopApp/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DesktopApp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,85 @@ namespace DesktopApp
 {
     public partial class Form1 : Form
     {
+        public string connectionString = @"Data Source=localhost;Initial Catalog=IOT;Integrated Security=True";
+
+
         public Form1()
         {
             InitializeComponent();
+
         }
+
+
+
+        public void ClearData()
+        {
+            nudTemparature.Value = 0;
+            nudHumidity.Value = 0;
+            nudPressure.Value = 1000;
+            nudLight.Value = 5000;
+        }
+
+        private void btnClearValues_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+
+        private void btnSetValues_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "INSERT INTO WeatherValues VALUES (@Temp, @Hum, @Pa, @Lux);";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    //command.Parameters.AddWithValue("@ID", len + 1);
+                    command.Parameters.AddWithValue("@Temp", nudTemparature.Value);
+                    command.Parameters.AddWithValue("@Hum", nudHumidity.Value);
+                    command.Parameters.AddWithValue("@Pa", nudPressure.Value);
+                    command.Parameters.AddWithValue("@Lux", nudLight.Value);
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+                LoadData();
+            }
+        }
+
+        private void LoadData()
+        {
+            List<WeatherValues> values = new List<WeatherValues>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM WeatherValues";
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    values.Add(new WeatherValues(
+                        (int)reader["ID"],
+                        (int)reader["Temperature"],
+                        (int)reader["Humidity"],
+                        (int)reader["Pressure"],
+                        (int)reader["Light"]));
+
+                }
+                reader.Close();
+                connection.Close();
+            }
+
+            dgvWeatherValues.DataSource = values;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+    
     }
 }
